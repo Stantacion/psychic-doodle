224589adbfb24467d1152af1e75cdcb9da1e43cc
Stantacion <37334954+Stantacion@users.noreply.github.com>
2022-01-12 11:55:07 +0200

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunniccorn app:app
---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, flash
+
+app = Flask(__name__)
+app.secret_key = "lilpeepfan"
+
+@app.route("/")
+def index():
+    flash("what's your name ?")
+    return render_template("index.html")
+
+@app.route("/greet", methods=["GET", "POST"])
+def greet():
+    flash("Hi " + request.form["name_input"] + ", great to see you!")
+    return render_template("index.html")
